d5c77e7b795211a9b7c077148175fd99159405c4
Jonathan Kenton <jonathankenton@gmail.com>
2018-08-10 17:55:48 +0300

    trivial

--- actions/settings.js
+++
@@ -1,9 +0,0 @@
-import { TOGGLE_SETTING } from './types'
-
-export function toggleSetting(settingName) {
-  console.log(settingName)
-  return {
-    type: TOGGLE_SETTING,
-    payload: settingName
-  }
-}
--- actions/types.js
+++
@@ -1,7 +0,0 @@
-/* Slider */
-export const GET_IMAGES_SUCCESS = 'get_images_success';
-export const SET_TRANSLATE_VALUE = 'set_translate_value';
-export const SET_INDEX = 'set_index';
-
-/* Settings */
-export const TOGGLE_SETTING = 'toggle_setting';
