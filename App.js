eb17bb1440b6bd3f43d6164b2451e27641b7066a
Jonathan Kenton <jonathankenton@gmail.com>
2018-08-10 17:20:06 +0300

    fetch data as get request instead of import

--- App.js
+++ App.js
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import Carousel from './components/carousel';
 import NavBar from './components/navBar';
 import './App.css';
-import dataSet from './moviesList.json'
+import axios from 'axios';
+
+const API = "./";
+const DEFAULT_QUERY = "moviesList.json";
 
 class MovieSlider extends Component {
 
   constructor() {
     super();
-    this.data = dataSet;
     this.state = {
+      isLoading: false,
+      error: null,
       position: 0,
-      moving: false
+      moving: false,
+      data: []
     };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    axios.get(API + DEFAULT_QUERY)
+      .then(result => this.setState({
+        data: result.data,
+        isLoading: false
+      }))
+      .catch(error => this.setState({
+        error,
+        isLoading: false
+      }));
   }
 
   positionCallback = (newPosition) => {
@@ -29,25 +48,30 @@ class MovieSlider extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <Carousel
-          data={this.data}
-          index={this.state.position}
-          moving={this.state.moving}
-          onChange={this.positionCallback}
-          onStop={this.movementCallback}
-        />
-        <NavBar
-          data={this.data}
-          index={this.state.position}
-          moving={this.state.moving}
-          onChange={this.positionCallback}
-          onStop={this.movementCallback}
-        />
-      </div>
-    )
+    if (!this.state.isLoading && (typeof this.state.data === "object") && (this.state.data.length > 0)) {
+      return (
+        <div>
+          <Carousel
+            data={this.state.data}
+            index={this.state.position}
+            moving={this.state.moving}
+            onChange={this.positionCallback}
+            onStop={this.movementCallback}
+          />
+          <NavBar
+            data={this.state.data}
+            index={this.state.position}
+            moving={this.state.moving}
+            onChange={this.positionCallback}
+            onStop={this.movementCallback}
+          />
+        </div>
+      )
+    } else {
+      return null;
+    }
   }
+
 }
 
 export default function App() {
