ad899dbe5762154745e127cbfaadc9b3ef56a38d
Jonathan Kenton <jonathankenton@gmail.com>
2018-08-10 11:40:28 +0300

    more code clean up

--- components/navBar/index.js
+++ components/navBar/index.js
@@ -13,25 +13,6 @@ const NavBar = ({ ...props }) => {
     list.forEach(function(item, index){
       item.index = index;
     });
-    // we need up to 5 items, with the current one in the middle
-   /* const currentItem = list[props.index];
-    const previousItems = list.slice(0, props.index);
-    const nextItems = list.slice(props.index + 1, list.length);
-    let nonCurrentItems = nextItems.concat(previousItems);
-    let allItems = [currentItem];
-    const itemsLength = nonCurrentItems.length;
-    if (itemsLength > 0){
-      allItems.unshift(nonCurrentItems[itemsLength - 1]);
-      if (itemsLength > 1){
-        allItems.push(nonCurrentItems[0]);
-        if (itemsLength > 2){
-          allItems.unshift(nonCurrentItems[itemsLength - 2]);
-          if (itemsLength > 4){
-            allItems.push(nonCurrentItems[1]);
-          }
-        }
-      }
-    }*/
     let listWithDuplicates = list.concat(list, list);
 
     return listWithDuplicates.map((value) => {
@@ -45,14 +26,8 @@ const NavBar = ({ ...props }) => {
       }
       const className = (isActive) ? "active" : "inactive";
 
-      const translateValue = 0//(props.index) * -100;
-
-      const styles = {
-        transform: `translateX(${translateValue}%)`,
-      };
-
       return (
-        <div key={value.index} className={className} onClick={() => setIndex(value.index)} style={styles}>{value.name}</div>
+        <div key={value.index} className={className} onClick={() => setIndex(value.index)} >{value.name}</div>
       )
     })
   };
@@ -60,8 +35,7 @@ const NavBar = ({ ...props }) => {
   const windowWidth = window.innerWidth;
   const itemWidth = windowWidth / 5;
   const navShift = (itemWidth * props.index) + windowWidth;
-
-
+  
   const navStyles = {
     transform: `translateX(-${navShift}px)`,
   };
